7924760b29125ac19e88fe2e4ccc35cb2989dfbb
Vagisha Kumar <vagishakumar433@gmail.com>
2025-02-19 22:40:59 +0530

    Merge branch "vagisha-master"

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 // import imageReducer from "./imagereducer";
 import { createStore, applyMiddleware } from 'redux';
+// import promiseMiddleware from "redux-promise"
+import promiseMiddleware from "redux-promise-middleware";
 
-import promiseMiddleware from "redux-promise"
+
 const { default: imageReducer } = require("./imagereducer");
 
 const store = createStore(
