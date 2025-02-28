eb08266450513a1f08db8064386a924948799071
Vagisha Kumar <vagishakumar433@gmail.com>
2025-02-28 10:51:48 +0530

    Endpoint enhance

--- src/Common/config.js
+++ src/Common/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const api_url = "https://image-editor-socialpilot-api.netlify.app/";
+export const api_url = "https://image-editor-socialpilot-api.netlify.app";
 
 export const apiRequest = (
   endpoint,
